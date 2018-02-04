57314be0770c9d82dfadfb35417d712618008342
Marcos Loureiro <mloureiro.dev@gmail.com>
2018-02-04 18:24:15 +0100

    Log activity actions

--- lib/Game/Service/ActivityService.dart
+++ lib/Game/Service/ActivityService.dart
@@ -4,14 +4,22 @@ import 'package:game/Game/Entity/Activity.dart';
 import 'package:game/Game/Service/GameClient.dart';
 import 'package:game/Game/Response/ActivityBonusCollectResponse.dart';
 import 'package:game/Game/Response/ActivityRewardCollectResponse.dart';
+import 'package:game/Infrastructure/Log.dart';
 
 class ActivityService {
+  static const _ACTION_FETCH = 'fetch_available';
+  static const _ACTION_START = 'start';
+  static const _ACTION_COLLECT = 'collect';
+
   final GameClient _client;
 
   ActivityService(this._client);
 
   Future<List<Activity>> getAvailableActivities() =>
-    _client.fetchPage('activities.html?tab=missions')
+    _log('fetch', _ACTION_FETCH, Log.debug)
+      .then((a) => _client.fetchPage('activities.html?tab=missions'))
+      .then((document) =>
+        _log('done', _ACTION_FETCH, Log.debug, result: document))
       .then(_client.extractHtml)
       .then((String html) =>
         new RegExp(r'data-d="(.*?)"')
@@ -19,24 +27,34 @@ class ActivityService {
           .map((Match match) => match.group(1))
           .toList())
       .then(_client.jsonListToMap)
-      .then(_makeActivityList);
+      .then(_makeActivityList)
+      .then((List<Activity> list) =>
+        _log('found $list', _ACTION_FETCH, Log.info, result: list));
 
   Future startActivity(Activity activity) =>
-    _client.performAction({
-      'class': 'Missions',
-      'action': 'start_mission',
-      'id_mission': activity.categoryId,
-      'id_member_mission': activity.id,
-    });
+    _log('execute $activity', _ACTION_START, Log.debug)
+      .then((a) => _client.performAction({
+        'class': 'Missions',
+        'action': 'start_mission',
+        'id_mission': activity.categoryId,
+        'id_member_mission': activity.id,
+      }))
+      .then((a) => _log('done $activity', _ACTION_START, Log.debug))
+      .then((a) => _log('started $activity', _ACTION_START, Log.info));
 
   Future<ActivityRewardCollectResponse> collectActivity(Activity activity) =>
-    _client.performAction({
-      'class': 'Missions',
-      'action': 'claim_reward',
-      'id_mission': activity.categoryId,
-      'id_member_mission': activity.id,
-    })
-      .then(_makeActivityRewardCollectResponse);
+    _log('execute $activity', _ACTION_COLLECT, Log.debug)
+      .then((a) => _client.performAction({
+        'class': 'Missions',
+        'action': 'claim_reward',
+        'id_mission': activity.categoryId,
+        'id_member_mission': activity.id,
+      }))
+      .then(_makeActivityRewardCollectResponse)
+      .then((reward) =>
+        _log('done $reward', _ACTION_COLLECT, Log.debug, result: reward))
+      .then((reward) =>
+        _log('collected $reward', _ACTION_COLLECT, Log.info, result: reward));
 
   Future collectBonus() =>
     _client.performAction({
@@ -72,4 +90,15 @@ class ActivityService {
   ActivityBonusCollectResponse _makeActivityBonusCollectResponse(Map json) =>
     new ActivityBonusCollectResponse(
       specialCurrency: json['changes']['hard_currency']);
+
+  Future _log(
+    String message,
+    String action,
+    Function callable,
+    { error, result }
+  ) async {
+    callable(message, context: ['activity', action], error: error);
+
+    return result;
+  }
 }
