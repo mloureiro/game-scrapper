b2501af9805e6b286762e617644a29f16a4b76cf
Marcos Loureiro <mloureiro.dev@gmail.com>
2018-02-18 22:37:56 +0100

    Make runner to handle arena fights

---
+++ lib/Game/Runner/ArenaRunner.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+import 'package:game/Game/Response/FightResponse.dart';
+import 'package:game/Game/Service/ArenaService.dart';
+import 'package:game/Game/Service/GameClientInterface.dart';
+import 'package:game/Infrastructure/Config.dart';
+import 'package:game/Infrastructure/Log.dart';
+
+class ArenaRunner {
+  static const _ARENAS_IDS = const [0, 1, 2];
+  static const _REFRESH_CHALLENGERS_TIME_IN_SECONDS = 30 * 60;
+  static const _CONFIG_FIGHT_KEY = 'arena.fight.next_run';
+
+  ArenaService _arenaService;
+  final Config _gameConfig;
+
+  ArenaRunner(GameClientInterface client, this._gameConfig) {
+    _arenaService = new ArenaService(client);
+  }
+
+  Future run() async {
+    if (!_isTimeToFight()) {
+      return;
+    }
+
+    for(int i = 0; i < _ARENAS_IDS.length; i++) {
+      await _fight(_ARENAS_IDS[i]);
+    }
+
+    _setNextFightRun();
+  }
+
+  Future<FightResponse> _fight(int id) =>
+    _arenaService.fetchChallenger(id)
+      .then(_arenaService.fight)
+      .catchError((error) =>
+        _log('arena fight #$id failed', 'fight', Log.warning, error: error));
+
+  void _setNextFightRun() =>
+    _gameConfig.set(
+      _CONFIG_FIGHT_KEY,
+      (_REFRESH_CHALLENGERS_TIME_IN_SECONDS * 1000)
+        + new DateTime.now().millisecondsSinceEpoch
+    );
+
+  bool _isTimeToFight() =>
+    _gameConfig.get(_CONFIG_FIGHT_KEY) == null
+      || _gameConfig.get(_CONFIG_FIGHT_KEY) < new DateTime.now().millisecondsSinceEpoch;
+
+  Future _log(
+    String message,
+    String action,
+    Function callable,
+    { error, result }
+  ) async {
+    callable(message, context: ['runner.arena', action], error: error);
+
+    return result;
+  }
+}
--- lib/Game/Runner/Runner.dart
+++ lib/Game/Runner/Runner.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:game/Game/Runner/ActivityRunner.dart';
+import 'package:game/Game/Runner/ArenaRunner.dart';
 import 'package:game/Game/Runner/PlayerRunner.dart';
 import 'package:game/Game/Runner/ShopRunner.dart';
 import 'package:game/Game/Runner/WorkerRunner.dart';
@@ -11,12 +12,14 @@ class Runner {
   final GameClientInterface _client;
   final Config _gameConfig;
   ActivityRunner _activityRunner;
+  ArenaRunner _arenaRunner;
   PlayerRunner _playerRunner;
   ShopRunner _shopRunner;
   WorkerRunner _workerRunner;
 
   Runner(this._client, this._gameConfig) {
     _activityRunner = new ActivityRunner(_client, _gameConfig);
+    _arenaRunner = new ArenaRunner(_client, _gameConfig);
     _playerRunner = new PlayerRunner(_client, _gameConfig);
     _shopRunner = new ShopRunner(_client, _gameConfig);
     _workerRunner = new WorkerRunner(_client, _gameConfig);
@@ -24,6 +27,7 @@ class Runner {
 
   Future run() async {
     await _activityRunner.run();
+    await _arenaRunner.run();
     await _playerRunner.run();
     await _workerRunner.run();
     await _shopRunner.run();
